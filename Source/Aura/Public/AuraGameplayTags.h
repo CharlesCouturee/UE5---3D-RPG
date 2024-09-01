64073c9e4f0b1a476e5725f711bbca230c02fd15
Charles Couture <charlescouture12@gmail.com>
2024-09-01 10:10:27 -0400

    LECTURE 86

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,24 @@
+// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * AuraGameplayTags
+ * 
+ * Singleton containing native GameplayTags
+ */
+
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return GameplayTags; }
+	static void InitializeNativeGameplayTags();
+
+protected:
+
+private:
+	static FAuraGameplayTags GameplayTags;
+};
