46d82f22508473d5a431f43190a96b70aeffa146
Charles Couture <charlescouture12@gmail.com>
2024-09-01 12:16:59 -0400

    LECTURE 90

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// Copyright Druid Mechanics
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
